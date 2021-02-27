3f7da48dfe26e1e321441ced4d6dfc11c72495cc
dlepperts <danielle.leppertsimenauer@gmail.com>
2021-02-27 13:12:39 -0600

    added props

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 class CoordinatesButton extends React.Component {
+    
+    handleClick = event => {
+        this.props.onReceiveCoordinates([event.clientX, event.clientY]);
+    };
 
+    
 }
 
 export default CoordinatesButton;
