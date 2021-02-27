9463e5469f3fd1383a3d9fae39e626380024e37e
dlepperts <danielle.leppertsimenauer@gmail.com>
2021-02-27 13:13:09 -0600

    added props

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -6,7 +6,9 @@ class CoordinatesButton extends React.Component {
         this.props.onReceiveCoordinates([event.clientX, event.clientY]);
     };
 
-    
+    render() {
+        return <button onClick={this.handleClick}>Coords</button>;
+    }
 }
 
 export default CoordinatesButton;
